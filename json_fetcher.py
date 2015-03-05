d4f73ed0e62baed6f23cc5c82db7f0fb7961f075
arank <arankhanna@college.harvard.edu>
2015-03-05 18:32:26 -0500

    added python json fetcher for blob file

--- json_fetcher.py
+++ json_fetcher.py
@@ -7,6 +7,15 @@ json_extract = re.search('(?<=\().+?(?=\))', response.read())
 data = json.loads(json_extract.group(0))
 
 # TODO extract important data or log document to DB?
+if data['success']:
+	string = ""
+	with open('data.txt', 'a') as outfile:
+		for v in data['vehicles']:
+			# Agency ID for harvard is 52
+			data_string = json.dumps(v)+','
+			string+=data_string
+		outfile.write(string)
+
 
 
 
